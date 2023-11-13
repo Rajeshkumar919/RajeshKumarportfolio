7043c73527818c6b48928eaf369f648fbb7d6c16
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-13 17:37:37 +0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,7 +4,7 @@
 // method 3: default return component
 
 // method 2:
-import profile from '../assets/images/zoya.jpg';
+import profile from '../assets/images/rajesh.jpg';
 export const HeroSection = () => {
     return(
         <>
