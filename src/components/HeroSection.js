2e848ca2d2c2833b1d41c0d36a2d822ae4edee75
Rajesh Kumar <60379726+Rajeshkumar919@users.noreply.github.com>
2023-11-13 17:34:10 +0500

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+// there are 3 ways to create a component
+// method 1: default functional component
+// method 2: arrow function component
+// method 3: default return component
+
+// method 2:
+import profile from '../assets/images/zoya.jpg';
+export const HeroSection = () => {
+    return(
+        <>
+            <div className="container hero-section">
+                <div className="row">
+                    <div className="col col-sm-12 col-md-6 col-lg-6">
+                        <h1 className="title-section">Hi, Rajesh Here!</h1>
+                        <h3 className="subheading">Computer Scientist</h3>
+                    </div>
+                    <div className="col col-sm-12 col-md-6 col-lg-6">
+                        <img src={profile} alt="profile"/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
